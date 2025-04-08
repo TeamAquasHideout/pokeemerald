85dae9871bc27651dc6681389b5a2a8364c6cc58
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2025-04-08 10:49:07 +0200

    fix target of ABILITY_IMPOSTER

--- src/battle_util.c
+++ src/battle_util.c
@@ -4459,17 +4459,20 @@ u32 AbilityBattleEffects(u32 caseID, u32 battler, u32 ability, u32 special, u32 
             }
             break;
         case ABILITY_IMPOSTER:
-            if (IsBattlerAlive(BATTLE_OPPOSITE(battler))
-                && !(gBattleMons[BATTLE_OPPOSITE(battler)].status2 & (STATUS2_TRANSFORMED | STATUS2_SUBSTITUTE))
+        {
+            u32 diagonalBattler = BATTLE_PARTNER(BATTLE_OPPOSITE(battler));
+            if (IsBattlerAlive(diagonalBattler)
+                && !(gBattleMons[diagonalBattler].status2 & (STATUS2_TRANSFORMED | STATUS2_SUBSTITUTE))
                 && !(gBattleMons[battler].status2 & STATUS2_TRANSFORMED)
-                && !(gBattleStruct->illusion[BATTLE_OPPOSITE(battler)].on)
-                && !(gStatuses3[BATTLE_OPPOSITE(battler)] & STATUS3_SEMI_INVULNERABLE_NO_COMMANDER))
+                && !(gBattleStruct->illusion[diagonalBattler].on)
+                && !(gStatuses3[diagonalBattler] & STATUS3_SEMI_INVULNERABLE_NO_COMMANDER))
             {
                 gBattlerAttacker = battler;
-                gBattlerTarget = BATTLE_OPPOSITE(battler);
+                gBattlerTarget = diagonalBattler;
                 BattleScriptPushCursorAndCallback(BattleScript_ImposterActivates);
                 effect++;
             }
+        }
             break;
         case ABILITY_MOLD_BREAKER:
             if (!gSpecialStatuses[battler].switchInAbilityDone)
