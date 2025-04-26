662784289472f01bbda87ea5b154d70812768ea7
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2025-04-26 20:27:24 +0200

    fixed Klutz interaction with Amulet Coin

--- src/battle_util.c
+++ src/battle_util.c
@@ -8508,7 +8508,8 @@ u32 GetBattlerHoldEffectInternal(u32 battler, bool32 checkNegating, bool32 check
             return HOLD_EFFECT_NONE;
         if (gFieldStatuses & STATUS_FIELD_MAGIC_ROOM)
             return HOLD_EFFECT_NONE;
-        if (checkAbility && GetBattlerAbility(battler) == ABILITY_KLUTZ)
+        if (checkAbility && GetBattlerAbility(battler) == ABILITY_KLUTZ
+          && gBattleMons[battler].item != ITEM_AMULET_COIN)
             return HOLD_EFFECT_NONE;
     }
 
