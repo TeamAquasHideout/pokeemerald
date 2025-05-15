fc1ca8b9ba0766f26f38e922f71b41c628f91431
wiz1989 <80073265+wiz1989@users.noreply.github.com>
2025-05-15 19:07:39 +0200

    flash fire recoil fix #6341

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -5622,6 +5622,7 @@ static void Cmd_moveend(void)
             else if (gMovesInfo[gCurrentMove].recoil > 0
                   && !(gMoveResultFlags & MOVE_RESULT_NO_EFFECT)
                   && IsBattlerAlive(gBattlerAttacker)
+                  && TARGET_TURN_DAMAGED
                   && gBattleScripting.savedDmg != 0) // Some checks may be redundant alongside this one
             {
                 gBattleMoveDamage = max(1, gBattleScripting.savedDmg * max(1, gMovesInfo[gCurrentMove].recoil) / 100);
