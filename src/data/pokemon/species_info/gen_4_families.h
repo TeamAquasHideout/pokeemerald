7dde868172c809dda23ae2cd2835adc373abe172
jonny <28536776+jonnysprojects@users.noreply.github.com>
2026-06-27 21:12:48 -0400

    Weird FIx

--- src/data/pokemon/species_info/gen_4_families.h
+++ src/data/pokemon/species_info/gen_4_families.h
@@ -6972,7 +6972,6 @@ const struct SpeciesInfo gSpeciesInfoGen4[] =
         FOOTPRINT(Heatran)
         SHADOW(-1, 0, SHADOW_SIZE_M)
         .isMegaEvolution = TRUE,
-        .isSubLegendary = TRUE,
         .perfectIVCount = LEGENDARY_PERFECT_IV_COUNT,
         .levelUpLearnset = sHeatranLevelUpLearnset,
         .teachableLearnset = sHeatranTeachableLearnset,
